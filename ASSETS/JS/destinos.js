a255b107f0e78aa8b7eee936bfae97f4c27de8c9
Luis_Zunino <zuninoluis01@gmail.com>
2021-03-12 16:59:41 -0300

    app.js
    se cargan imagenes y otros archivos

---
+++ ASSETS/JS/destinos.js
@@ -0,0 +1,396 @@
+class viaje {
+  constructor(tipo, img, departamento, nombre, costo, info) {
+    this.tipo = tipo;
+    this.img = img;
+    this.departamento = departamento;
+    this.nombre = nombre;
+    this.costo = costo;
+    this.info = info;
+  }
+}
+const viaje1 = new viaje(
+  "ciudad",
+  "2",
+  "Montevideo",
+  "Montevideo",
+  500,
+  "Capital de Uruguay conocida por la Ciudad Vieja colonial y art deco, y playas como Pocitos."
+);
+const viaje2 = new viaje(
+  "ciudad",
+  "3",
+  "Maldonado",
+  "Punta del Este",
+  500,
+  "Ciudad turística uruguaya conocida por la escultura “La Mano”, la playa Mansa y la isla Gorriti."
+);
+const viaje3 = new viaje(
+  "ciudad",
+  "4",
+  "Colonia",
+  "Colonia del Sacramento",
+  500,
+  "Ciudad uruguaya frente al río con su Barrio Histórico bien conservado desde la época colonial"
+);
+const viaje4 = new viaje(
+  "ciudad",
+  "6",
+  "Colonia",
+  "Río de la Plata",
+  500,
+  "Estuario de los ríos Paraná y Uruguay"
+);
+const viaje5 = new viaje(
+  "playa",
+  "7",
+  "Rocha",
+  "Punta del Diablo",
+  500,
+  "Pueblo turístico uruguayo conocido por sus playas, el parque Santa Teresa y la conservación de tortugas."
+);
+const viaje6 = new viaje(
+  "playa",
+  "7",
+  "Maldonado",
+  "Piriápolis",
+  500,
+  " Población costera de Uruguay con vistas desde el cerro de San Antonio y edificios de estilo Belle Époque."
+);
+const viaje7 = new viaje(
+  "ciudad",
+  "8",
+  "Durazno",
+  "José Ignacio",
+  500,
+  "Complejo turístico playero elegante con áreas para surf, coctelerías y restaurantes modernos, además de vistas desde el faro de José Ignacio"
+);
+const viaje8 = new viaje(
+  "ciudad",
+  "9",
+  "Salto",
+  "Salto",
+  500,
+  "Ciudad uruguaya conocida por la animada calle Uruguay, el Teatro Larrañaga del siglo XIX y las aguas termales."
+);
+const viaje9 = new viaje(
+  "ciudad",
+  "10",
+  "Rocha",
+  "La Paloma",
+  500,
+  "Pueblo costero de Uruguay junto al faro del cabo de Santa María, la bahía Grande y la reserva Laguna de Rocha."
+);
+const viaje10 = new viaje(
+  "ciudad",
+  "11",
+  "Rocha",
+  "Departamento de Rocha",
+  500,
+  "Departamento del este de Uruguay con playas en la Punta del Diablo y tortugas marinas en La Coronilla."
+);
+const viaje11 = new viaje(
+  "playa",
+  "12",
+  "Maldonado",
+  "La Pedrera",
+  500,
+  "Playa, surf y deportes acuáticos"
+);
+const viaje12 = new viaje(
+  "playa",
+  "13",
+  "Carmelo",
+  "Colonia",
+  500,
+  "Municipio uruguayo famoso por la Playa Seré, el Río de la Plata y la Rambla de los Constituyentes"
+);
+const viaje13 = new viaje(
+  "playa",
+  "14",
+  "Rocha",
+  "Rocha",
+  500,
+  "Playa y parque"
+);
+const viaje14 = new viaje(
+  "ciudad",
+  "15",
+  "desconocido",
+  "Fray Bentos",
+  500,
+  "Teatro, museo, historia, playa y río"
+);
+const viaje15 = new viaje(
+  "ciudad",
+  "16",
+  "Rocha",
+  "Chuy",
+  500,
+  "Parque acuático, playa, compras y parque"
+);
+const viaje16 = new viaje(
+  "ciudad",
+  "17",
+  "Canelones",
+  "Atlántida",
+  500,
+  "Playa, águila y jardín zoológico"
+);
+const viaje17 = new viaje(
+  "ciudad",
+  "18",
+  "Rocha",
+  "Barra de Valizas",
+  500,
+  "Playa"
+);
+const viaje18 = new viaje(
+  "ciudad",
+  "19",
+  "Rocha",
+  "Termas del Arapey",
+  500,
+  "Aguas termales y golf"
+);
+const viaje19 = new viaje(
+  "ciudad",
+  "20",
+  "Rocha",
+  "Garzón",
+  500,
+  "Pueblo uruguayo conocido por el restaurante El Garzón, la bodega Garzón y las playas de José Ignacio."
+);
+const viaje20 = new viaje(
+  "playa",
+  "21",
+  "Maldonado",
+  "Maldonado",
+  500,
+  "Playa, museo e historia"
+);
+const viaje21 = new viaje(
+  "playa",
+  "22",
+  "Lavalleja",
+  "Minas",
+  500,
+  "Parque y naturaleza"
+);
+const viaje22 = new viaje(
+  "ciudad",
+  "23",
+  "Rivera",
+  "Rivera",
+  500,
+  "Casino, bodega, compras y vino"
+);
+const viaje23 = new viaje(
+  "ciudad",
+  "24",
+  "Colonia",
+  "Nueva Helvecia",
+  500,
+  "Historia, monumento y cata de vinos"
+);
+const viaje24 = new viaje(
+  "ciudad",
+  "25",
+  "Rocha",
+  "San Gregorio de Polanco",
+  500,
+  "Playa, pintura, arte y museo"
+);
+const viaje25 = new viaje(
+  "ciudad",
+  "26",
+  "Tacuarembó",
+  "Tacuarembó",
+  500,
+  "Museo, arte y naturaleza"
+);
+const viaje26 = new viaje(
+  "ciudad",
+  "27",
+  "Maldonado",
+  "Pan de Azúcar",
+  500,
+  "Turismo ecológico, parque e historia"
+);
+const viaje27 = new viaje(
+  "ciudad",
+  "28",
+  "Lavalleja",
+  "Villa Serrana",
+  500,
+  "Senderismo"
+);
+const viaje28 = new viaje(
+  "ciudad",
+  "29",
+  "Maldonado",
+  "Sauce de Portezuelo",
+  500,
+  "Playa"
+);
+const viaje29 = new viaje(
+  "ciudad",
+  "30",
+  "Paysandú",
+  "Paysandú",
+  500,
+  "Ciudad uruguaya con el museo histórico, esculturas en el cementerio de Paysandú y baños termales cercanos."
+);
+const viaje30 = new viaje(
+  "ciudad",
+  "31",
+  "",
+  "Termas del Dayman",
+  500,
+  "Aguas termales y parque acuático"
+);
+const viaje31 = new viaje(
+  "ciudad",
+  "32",
+  "Canelones",
+  "Canelones",
+  500,
+  "Bodega, playa, viña, vino y cata de vinos"
+);
+const viaje32 = new viaje(
+  "ciudad",
+  "33",
+  "Maldonado",
+  "Laguna Garzón",
+  500,
+  "Área protegida, kitesurf y windsurf"
+);
+const viaje33 = new viaje(
+  "ciudad",
+  "34",
+  "",
+  "Manantiales",
+  500,
+  "Pueblo animado frente al mar, con surf en la playa Bikini, una intensa vida nocturna, y tiendas y restaurantes modernos."
+);
+const viaje34 = new viaje(
+  "ciudad",
+  "35",
+  "Durazno",
+  "Durazno",
+  500,
+  "Jardín zoológico, monumento e historia"
+);
+const viaje35 = new viaje(
+  "ciudad",
+  "36",
+  "Paysandú",
+  "Paysandú",
+  500,
+  "Monumento, basílica y aguas termales"
+);
+const viaje36 = new viaje(
+  "ciudad",
+  "37",
+  "San José",
+  "San José de Mayo",
+  500,
+  "Catedral, museo e historia"
+);
+const viaje37 = new viaje(
+  "ciudad",
+  "38",
+  "Treinta y Tres",
+  "Treinta y Tres",
+  500,
+  "Naturaleza, río e historia"
+);
+const viaje38 = new viaje("ciudad", "39", "", "Aguas Dulces", 500, "Playa");
+const viaje39 = new viaje(
+  "ciudad",
+  "40",
+  "",
+  "Punta Colorada",
+  500,
+  "Playa y surf"
+);
+const viaje40 = new viaje(
+  "ciudad",
+  "41",
+  "Artigas",
+  "Artigas",
+  500,
+  "Minería, monumento y naturaleza"
+);
+const viaje41 = new viaje(
+  "ciudad",
+  "42",
+  "Rocha",
+  "Laguna de Rocha",
+  500,
+  "Área protegida, humedal y naturaleza"
+);
+
+const viajes = [
+  viaje1,
+  viaje2,
+  viaje3,
+  viaje4,
+  viaje5,
+  viaje6,
+  viaje7,
+  viaje8,
+  viaje9,
+  viaje10,
+  viaje11,
+  viaje12,
+  viaje13,
+  viaje14,
+  viaje15,
+  viaje16,
+  viaje17,
+  viaje18,
+  viaje19,
+  viaje20,
+  viaje21,
+  viaje22,
+  viaje23,
+  viaje24,
+  viaje25,
+  viaje26,
+  viaje27,
+  viaje28,
+  viaje29,
+  viaje30,
+  viaje31,
+  viaje32,
+  viaje32,
+  viaje34,
+  viaje35,
+  viaje36,
+  viaje37,
+  viaje38,
+  viaje39,
+  viaje40,
+  viaje41,
+];
+for (const viaje of viajes) {
+  let departamento = viaje.departamento;
+  let img = viaje.img;
+  let nombre = viaje.nombre;
+  let costo = viaje.costo;
+  let info = viaje.info;
+  document.getElementById("viajes").innerHTML += `
+  <ul>
+    <li  class="opcionDestino">
+      <img src="ASSETS/IMG/DESTINOS/unnamed(${img}).jpg">
+      <div> 
+        <h1>${nombre}</h1>
+        <span>
+            <p>Departamento: ${departamento}</p>
+            <p>Precio por noche: ${costo}</p>
+            <p>${info}</p> 
+        </span>
+      </div>
+    </li>
+  </ul>`;
+}
