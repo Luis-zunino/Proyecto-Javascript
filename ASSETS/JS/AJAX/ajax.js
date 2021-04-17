a8fc11bf77d99e909e689436aca30481474c2073
Luis_Zunino <zuninoluis01@gmail.com>
2021-04-16 21:04:56 -0300

    ajax.js
    se modifica para utilizar ajax

---
+++ ASSETS/JS/AJAX/ajax.js
@@ -0,0 +1,52 @@
+let contenidoGeneral = [];
+
+$(document).ready(() => {
+  $("#grillaDeViajes").show("slow", () => {
+    $.getJSON("ASSETS/JS/AJAX/viajes.json", (response, status) => {
+      if (status === "success") {
+        let contenido = response;
+        contenidoGeneral = contenido;
+        for (const destino of contenido) {
+          $("#contenido").append(cargoDestino(destino));
+        }
+      } else {
+        $("#contenido").html(contenidoError);
+      }
+      $("#contenido").fadeIn("slow");
+    });
+  });
+});
+
+const contenidoError = `<div class="center red-text">
+                            <h4 >No se pudo recuperar el contenido</h4>
+                            <h5>Intente nuevamente en unos segundos...</h5>
+                        </div>`;
+
+// const verDetalle = (id) => {
+//   const destino = contenidoGeneral.find((c) => c.id == id);
+//   localStorage.detalleFilm = JSON.stringify(destino);
+//   location.target = "_self";
+//   location.href = "detalle.html";
+// };
+
+const cargoDestino = (destino) => {
+  let streaming = JSON.stringify(destino);
+  if (destino != undefined) {
+    const cargoViajes = `
+    <ul>
+  <li  class="opcionDestino">
+    <img src="ASSETS/IMG/DESTINOS/unnamed(${destino.img}).jpg">
+    <div> 
+      <h1>${destino.nombre}</h1>
+      <span class="contViaje">
+          <p>Departamento: ${destino.departamento}</p>
+          <p>Precio por noche: ${destino.costo}</p>
+          <p>${destino.info}</p> 
+      </span>
+      <button onclick="agregoViajeAlCarrito(${destino.id})">Agregar</button>
+    </div>
+  </li>
+</ul>               `;
+    return cargoViajes;
+  }
+};
